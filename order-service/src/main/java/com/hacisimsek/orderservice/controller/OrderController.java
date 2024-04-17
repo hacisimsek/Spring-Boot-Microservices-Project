0d35bc25b912987aacfe438bec2ce6f4c70459bc
Hacı Şimşek <haci.simsek@sisal.com>
2024-04-17 13:06:38 +0300

    Added Git configuration files and updated application.properties for order-service, product-service, and inventory-service

--- order-service/src/main/java/com/hacisimsek/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/hacisimsek/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.hacisimsek.orderservice.controller;
 import com.hacisimsek.orderservice.dto.OrderRequest;
 import com.hacisimsek.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void placeOrder(@RequestBody OrderRequest orderRequest){
-         orderService.placeOrder(orderRequest);
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+
+        if(orderService.placeOrder(orderRequest)){
+            return "Order placed successfully";
+        }else{
+            return"Order failed";
+        }
     }
 }
